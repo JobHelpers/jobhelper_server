5c0761cc98655c76b49a627552cbce9e673b73f0
NazarBylen <nazar.bylen.ir.2021@lpnu.ua>
2022-05-13 13:55:28 +0300

    created subjects & specialities_subject end up

--- app/mod_specialities_subject/models.py
+++ app/mod_specialities_subject/models.py
@@ -1,12 +1,13 @@
+import enum
+
 from sqlalchemy import ForeignKey
 
 from app import db
-from app.mod_specialities.models import Speciality
 
 class Speciality_subject(db.Model):
     __tablename__ = 'specialities_subjects'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     speciality_code = db.Column(db.String(10), ForeignKey('specialities.id'))
-    subject_id = db.Column(db.Integer, unique=True, nullable=False)
-    subject_status = db.Column(db.Enum, nullable=False)
+    subject_id = db.Column(db.Integer, ForeignKey('subjects.id'))
+    subject_status = db.Column(db.Enum('main', 'mandatory', 'optional'), nullable=False)
 
