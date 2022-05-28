09050d9d76b6ed2bef4b18c968693ca3d1bdd524
Ihor Byra <ihorbyra@gmail.com>
2022-05-28 17:57:12 +0300

    fixed Foreign Key

--- app/mod_specialities_subject/models.py
+++ app/mod_specialities_subject/models.py
@@ -5,7 +5,7 @@ from app import db
 class Speciality_subject(db.Model):
     __tablename__ = 'specialities_subjects'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    speciality_code = db.Column(db.String(10), ForeignKey('specialities.id'))
+    speciality_code = db.Column(db.String(10), ForeignKey('specialities.code'))
     subject_id = db.Column(db.Integer, ForeignKey('subjects.id'))
     subject_status = db.Column(db.Enum('main', 'mandatory', 'optional', name='subject_status'), nullable=False)
 
