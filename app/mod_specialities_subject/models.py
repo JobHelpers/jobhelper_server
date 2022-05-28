35bd398865628467dd50b5fa36c96bb657daaae7
Ihor Byra <ihorbyra@gmail.com>
2022-05-28 17:50:31 +0300

    setname for  enum field

--- app/mod_specialities_subject/models.py
+++ app/mod_specialities_subject/models.py
@@ -7,5 +7,5 @@ class Speciality_subject(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     speciality_code = db.Column(db.String(10), ForeignKey('specialities.id'))
     subject_id = db.Column(db.Integer, ForeignKey('subjects.id'))
-    subject_status = db.Column(db.Enum('main', 'mandatory', 'optional'), nullable=False)
+    subject_status = db.Column(db.Enum('main', 'mandatory', 'optional'), nullable=False, name='subject_status')
 
