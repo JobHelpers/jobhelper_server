0f8a0fb49309736667162ebc6db6b875e5b91294
ihor-patko <patkoihor2004@gmail.com>
2022-06-03 19:40:02 +0300

    findSubject

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,12 @@ db = SQLAlchemy(app)
 
 # Import a modules using its blueprint handler variable
 from app.mod_collaborators.controllers import mod_collaborators
+from app.mod_subjects.controllers import mod_subjects
+from app.mod_cities.controllers import mod_cities
 
 # Register blueprint(s)
 app.register_blueprint(mod_collaborators, url_prefix='/api')
+app.register_blueprint(mod_subjects, url_prefix='/api/subjects')
+app.register_blueprint(mod_subjects, url_prefix='/api/cities')
 
 db.create_all()
