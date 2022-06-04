03a530e79ea8b6ef3adb09f8005ed52031d741b5
ihor-patko <patkoihor2004@gmail.com>
2022-06-04 15:39:59 +0300

    fixing mistakes

--- app/__init__.py
+++ app/__init__.py
@@ -8,12 +8,10 @@ db = SQLAlchemy(app)
 
 # Import a modules using its blueprint handler variable
 from app.mod_collaborators.controllers import mod_collaborators
-from app.mod_subjects.controllers import mod_subjects
 from app.mod_cities.controllers import mod_cities
 
 # Register blueprint(s)
 app.register_blueprint(mod_collaborators, url_prefix='/api')
-app.register_blueprint(mod_subjects, url_prefix='/api/subjects')
-app.register_blueprint(mod_subjects, url_prefix='/api/cities')
+app.register_blueprint(mod_cities, url_prefix='/api')
 
 db.create_all()
