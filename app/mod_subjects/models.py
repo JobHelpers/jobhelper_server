66b279c989905f01bd38e77c82a036170551d788
ihor-patko <91693589+ihor-patko@users.noreply.github.com>
2022-06-04 15:15:08 +0300

    Deleting conflict directory.

--- app/mod_subjects/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import relationship
-
-from app import db
-
-
-class Subjects(db.Model):
-    __tablename__ = 'subjects'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(127), nullable=False)
