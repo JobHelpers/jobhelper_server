bc3ca03d9764380a16f24e68ce67686cd91c9fd7
maks morochenets <maksmorroo@gmail.com>
2022-06-07 19:39:41 +0300

    added controller `faculties` and changed model

---
+++ app/mod_faculties/controllers.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from sqlalchemy import and_
+
+from app import db
+from app.mod_faculties.models import Faculties
+
+mod_faculties = Blueprint('faculties', __name__, url_prefix='/faculties')
+
+
+@mod_faculties.route('/faculties', methods=['GET'])
+def show_faculties():
+    university_id = request.args.get('university_id')
+    speciality_code = request.args.get('speciality_code')
+
+    result = db.Query(Faculties).filter(and_(Faculties.university_id == university_id,
+                                             Faculties.speciality_code == speciality_code)).all()
+
+    data = [{
+        'id': row.id,
+        'name': row.name,
+        'speciality_code': row.speciality_code,
+        'university_id': row.university_id,
+        'short_description': row.short_description,
+        'url': row.url
+    } for row in result]
+
+    return jsonify(data)
--- app/mod_faculties/models.py
+++ app/mod_faculties/models.py
@@ -3,13 +3,11 @@ from sqlalchemy.orm import relationship
 from app import db
 
 
-class Collaborator(db.Model):
+class Faculties(db.Model):
     __tablename__ = 'faculties'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.text, unique=False, nullable=False)
-    speciality_code = db.Column(db.String(100), unique=True, nullable=False)
-    university_id = db.Column(db.Integer, unique=True, nullable=False)
-    short_description = db.Column(db.text, unique=False, nullable=False)
-    url = db.Column(db.text, unique=False, nullable=False)
-    fir_children = relationship("Specialities")
-    sec_children = relationship("Universities")
+    name = db.Column(db.Text, nullable=False, unique=False)
+    university_id = db.Column(db.Integer, ForeignKey('universities.id'), unique=False)
+    speciality_code = db.Column(db.String(100), ForeignKey('specialities.code'), unique=False)
+    short_description = db.Column(db.Text, unique=False, nullable=False)
+    url = db.Column(db.Text, unique=False, nullable=False)
