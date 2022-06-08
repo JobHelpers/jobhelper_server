e92b677243356c476a081d9f859946324c5d8331
Orest0501 <oborshovskiy@gmail.com>
2022-06-08 11:44:31 +0300

    Fixed universities

--- app/mod_universities/models.py
+++ app/mod_universities/models.py
@@ -5,6 +5,6 @@ from sqlalchemy import ForeignKey
 class University(db.Model):
     __tablename__ = 'universities'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.tinytext, unique=False, nullable=False)
-    city_id = db.Column(db.Integer, unique=True, nullable=False, ForeignKey('cities.id'))
+    name = db.Column(db.Text, unique=False, nullable=False)
+    city_id = db.Column(db.Integer, ForeignKey('cities.id'))
 
