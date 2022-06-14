345e8fd73addd338221be3d0e86d66a44a3f542e
Ihor Byra <ihorbyra@gmail.com>
2022-06-14 19:20:09 +0300

    Added Flask extension for handling Cross Origin Resource Sharing (CORS)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('config')
 
 db: SQLAlchemy = SQLAlchemy(app)
