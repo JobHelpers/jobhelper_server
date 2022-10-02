1618f6fa4e1cf2e2a39da30208b93845af81122a
unknown <nazar.bylen.ir.2021@lpnu.ua>
2022-10-02 14:18:51 +0300

    model coefficients updated

--- app/mod_coefficients/models.py
+++ app/mod_coefficients/models.py
@@ -4,6 +4,7 @@ from app import db
 class Coefficients(db.Model):
     __tablename__ = 'coefficients'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    coefficient_code = db.Column(db.float)
+    coefficient_code = db.Column(db.float, nullable=False)
+    min_grade = db.Column(db.Integer, nullable=False)
     subject_id = db.Column(db.Integer, ForeignKey('subjects.id'))
     faculty_id = db.Column(db.Integer, ForeignKey('faculties.id'))
